0f7d7b6c0af9f4eeda607c0754789b73e5814942
Alice Cecile <alice.i.cecile@gmail.com>
2016-05-22 13:43:09 -0400

    Reorganization

--- Tools/clustering.R
+++ Tools/clustering.R
@@ -1,22 +1,6 @@
 # Dependencies ####
 source("./Tools/distance_computation.R")
 
-# Wrapper ####
-
-find_clusters <- function(data, markov_kernel, 
-                          method, distance_type = "hitting_time", symmetrization_method = "none",
-                          ...){
-    
-    clusters <- switch(method,
-                            "blockmodel" = clust_blockmodel(data, markov_kernel, ...),
-                            "flow" = clust_flow(data, markov_kernel, ...),
-                            "kmeans" = clust_kmeans(data, markov_kernel, distance_type, symmetrization_method, ...)
-                       )
-    
-    return(clusters)
-}
-
-
 # Clustering methods ####
 
 clust_blockmodel <- function(data, markov_kernel, ...) {
@@ -27,6 +11,18 @@ clust_flow <- function(data, markov_kernel, ...) {
     
 }
 
-clust_kmeans <- function(data, markov_kernel, distance_type, symmetrization_method, ...) {
+# Wrapper ####
+
+find_clusters <- function(markov_kernel, 
+                          method, distance_type = "hitting_time",
+                          ...){
     
+    clusters <- switch(method,
+                            "blockmodel" = clust_blockmodel(data, markov_kernel, ...),
+                            "flow" = clust_flow(data, markov_kernel, ...)
+                       )
+    
+    return(clusters)
 }
+
+
